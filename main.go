aaed90632df3b81d3fc9558f54ce5d7ef3d5092f
NamLH2 <namlh2@vetc.com.vn>
2023-11-08 09:47:57 +0700

    test

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"LotusPart2/conf"
 	"LotusPart2/pkg/route"
 	"context"
+	"fmt"
 	"os"
 
 	"gitlab.com/goxp/cloud0/logger"
@@ -42,4 +43,84 @@ func main() {
 		logger.Tag("main").Error(err)
 	}
 	os.Clearenv()
+}
+
+// renew all elements that last e plus 1
+// [0,2,3] => [0,2,4]
+// [0,9] => [0,1,0]
+func solution1(input []int64) []int64 {
+
+	var lastSliceValue = input[len(input)-1]
+	if lastSliceValue < 9 {
+		input[len(input)-1] = lastSliceValue + 1
+		return input
+	}
+
+	var tmpSlice = make([]int64, len(input)+1)
+	for i, v := range input {
+		if i == len(input)-1 {
+			tmpSlice[i] = 1
+			tmpSlice[i+1] = 0
+		} else {
+			tmpSlice[i] = v
+		}
+	}
+
+	return tmpSlice
+}
+
+func solution2(input [][]int64) int64 {
+	var resp int64
+
+	var hmap = map[int]int64{}
+
+	for i, v1 := range input {
+		for _, v2 := range v1 {
+			hmap[i] += v2
+		}
+	}
+
+	for v := range hmap {
+		if hmap[v] > resp {
+			resp = hmap[v]
+		}
+	}
+
+	return resp
+}
+
+// jewels-and-stones
+func solution3(jewels, stone string) int64 {
+	var resp int64
+
+	var hmap = make(map[string]bool, len(jewels))
+
+	for _, v := range jewels {
+		hmap[fmt.Sprintf("%c", v)] = true
+	}
+
+	for _, v := range stone {
+		if _, ok := hmap[fmt.Sprintf("%c", v)]; ok {
+			resp++
+		}
+	}
+
+	return resp
+}
+
+// find the index of elements that total = k
+func solution4(input []int64, k int64) (index []int64) {
+	var hmap = make(map[int64]int64)
+
+	for i, v := range input {
+		if indexHmap, ok := hmap[k-v]; !ok {
+			hmap[v] = int64(i)
+		} else {
+			index = append(index, indexHmap)
+			index = append(index, int64(i))
+			break
+		}
+	}
+
+	return index
 }
